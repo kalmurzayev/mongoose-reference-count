276a2fc14a428f16f4ca688ec9c208d1749c96a6
kalmurzayev <azamat@utexas.edu>
2014-04-18 22:29:54 +0600

    first stable version

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var mongoose = require('mongoose');
 var postFind = require('mongoose-post-find');
 
 module.exports = function (schema, options) {
-	var hitsType = Number;
+	var hitsType = {
+            type: Number,
+            default: 0,
+            index: true
+        };
 
 	schema.add({
 		hits: hitsType
 	});
+	schema.plugin(postFind, {
+        findOne: function (result, done) {
+            if (result) {
+                result.hits = (!result.hits) ? 1 : result.hits + 1;
+                result.save();
+            }
+            done(null, result);
+        }
+    });
 }
