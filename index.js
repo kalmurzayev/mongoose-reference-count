c1e414aee6fd843182df5fdcb2eb2d3bd6d66370
kalmurzayev <azamat@utexas.edu>
2014-04-24 16:59:26 +0600

    added erro handler

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var postFind = require('mongoose-post-find');
 
 module.exports = function (schema, options) {
-	var hitsType = {
-            type: Number,
-            default: 0,
-            index: true
-        };
+    var hitsType = {
+        type: Number,
+        default: 0,
+        index: true
+    };
 
-	schema.add({
-		hits: hitsType
-	});
-	schema.plugin(postFind, {
+    schema.add({
+        hits: hitsType
+    });
+    schema.plugin(postFind, {
         findOne: function (result, done) {
             if (result) {
                 result.hits = (!result.hits) ? 1 : result.hits + 1;
-                result.save();
+                result.save(function (err, argument) {
+                    if (err) {
+                        console.log('MongooseReferenceCount: Failed to update count')
+                        console.log(err);
+                    }
+                });
             }
             done(null, result);
         }
